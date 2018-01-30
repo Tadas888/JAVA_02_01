ebadbc827b2592f9f1c970aa295c0f87576a02d3
Tadas888 <T_zukauskas@yahoo.com>
2018-01-30 14:34:23 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite savo varda:");
+        String vardas = sc.nextLine();
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
+        System.out.println(vardas);
     }
 }
